437e696ffecd7be79fbe9d2529e53b50c4106190
erasiaputri <s21610538@student.unklab.ac.id>
2020-09-29 20:07:41 +0800

    9th Commit

--- pertemuan_11/exercise.js
+++ pertemuan_11/exercise.js
@@ -1,23 +1,25 @@
 //Exercise
 //Convert to ES6 Syntax
 
-function yearUntilRetirement (year, firstName){
-    var age = 28;
-    var retirement = 65 - age;
-    if (retirement > 0) {
-        console.log(firstName + " retired in " + retirement + "years");
-    }else{
-        console.log(firstName + " is already retired.");
-    }
-}
+// function yearUntilRetirement (year, firstName){
+//     var age = 28;
+//     var retirement = 65 - age;
+//     if (retirement > 0) {
+//         console.log(firstName + " retired in " + retirement + "years");
+//     }else{
+//         console.log(firstName + " is already retired.");
+//     }
+// }
 
 //ES6
 const yearUntilRetirement = (year, firstName) => {
-    let age = 28;
+    let age = 2020 - year;
     let retirement = 65 - age;
-    if (retirement > 0) {
-        console.log(`${firstName} retired in ${retirement} years`);
+    if (retirement < 66) {
+        return (`${firstName} retired in ${retirement} years`);
     }else{
-        console.log(`${firstName} is already retired.`);
+        return (`${firstName} is already retired.`);
     }
-}
+};
+
+console.log(yearUntilRetirement(1999,`Putri`));
